36399961d130c67a28706c31e54c771f74f8d07d
Stephen Carter <stephen.carter@salesforce.com>
2025-01-07 12:01:22 -0500

    Code for v2

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,9 @@
 import { EnvironmentVariables } from './types'
-import * as path from 'path'
 
 /**
- * Returns a copy of the current process environment variables with the supplied environment variables added
+ * Returns a copy of the current process environment variables with the supplied environment variables added.
+ * Any supplied environment variables are only added if they do not already exist. This means we give preference to
+ * the values for the variables that the user has set always.
  */
 export function mergeWithProcessEnvVars(envVars: EnvironmentVariables): EnvironmentVariables {
     const mergedEnvVars: EnvironmentVariables = {}
@@ -18,26 +19,55 @@ export function mergeWithProcessEnvVars(envVars: EnvironmentVariables): Environm
 }
 
 /**
- * Extracts the value of the outfile from the run arguments or returns an empty string if not found
+ * Utility class to help ask questions regarding the users input arguments
  */
-export function extractOutfileFromRunArguments(runArgs: string): string {
-    const spaceMarker = '<<SPACE>>'
-    let markedRunArgs: string = markSpacesBetweenQuotes(runArgs, spaceMarker)
-    markedRunArgs = markedRunArgs.replace(/ +/g, ' ')
-    const parts: string[] = markedRunArgs.split(' ')
+export class InputArguments {
+    private readonly inputArgs: InputArgument[]
+    constructor(argumentsText: string) {
+        this.inputArgs = extractInputArguments(argumentsText)
+    }
+
+    containsFlag(longFlag: string, shortFlag?: string): boolean {
+        const flagsToMatch: string[] = [longFlag, ...(shortFlag ? [shortFlag] : [])]
+        return this.inputArgs.some(ia => flagsToMatch.includes(ia.flag))
+    }
+
+    getValuesFor(longFlag: string, shortFlag?: string): string[] {
+        const flagsToMatch: string[] = [longFlag, ...(shortFlag ? [shortFlag] : [])]
+        return this.inputArgs
+            .filter(ia => flagsToMatch.includes(ia.flag) && ia.value !== undefined)
+            .map(ia => ia.value as string)
+    }
+}
+type InputArgument = {
+    flag: string
+    value?: string
+}
+function extractInputArguments(argumentsText: string): InputArgument[] {
+    const inputArgs: InputArgument[] = []
+
+    const parts: string[] = markCharBetweenQuotes(argumentsText.trim(), ' ', '<<SPACE>>')
+        .replace(/ +/g, ' ')
+        .split(' ')
+        .map(p => p.replaceAll('<<SPACE>>', ' '))
+
     for (let i = 0; i < parts.length; i++) {
-        const partLower = parts[i].toLowerCase()
-        if ((partLower === '-o' || partLower === '--outfile') && i < parts.length - 1) {
-            return parts[i + 1].replaceAll(spaceMarker, ' ')
-        } else if (partLower.startsWith('-o=')) {
-            return parts[i].substring(3)
-        } else if (partLower.startsWith('--outfile=')) {
-            return parts[i].substring(10)
+        if (i === parts.length - 1 || parts[i + 1].startsWith('-')) {
+            if (parts[i].includes('=')) {
+                const idx: number = parts[i].indexOf('=')
+                inputArgs.push({ flag: parts[i].toLowerCase().substring(0, idx), value: parts[i].substring(idx + 1) })
+            } else {
+                inputArgs.push({ flag: parts[i].toLowerCase() })
+            }
+        } else {
+            inputArgs.push({ flag: parts[i].toLowerCase(), value: parts[i + 1] })
+            i++
         }
     }
-    return ''
+
+    return inputArgs
 }
-function markSpacesBetweenQuotes(value: string, spaceMarker: string): string {
+function markCharBetweenQuotes(value: string, charToFind: string, markerToReplaceCharWith: string): string {
     let insideQuotes = false
     let currentQuote = '"'
     let output = ''
@@ -47,16 +77,11 @@ function markSpacesBetweenQuotes(value: string, spaceMarker: string): string {
             currentQuote = c
         } else if (insideQuotes && c === currentQuote) {
             insideQuotes = false
-        } else if (insideQuotes && c === ' ') {
-            output += spaceMarker
+        } else if (insideQuotes && c === charToFind) {
+            output += markerToReplaceCharWith
         } else {
             output += c
         }
     }
     return output
-}
-
-const pwd: string = path.resolve('.') + path.sep
-export const toRelativeFile = (file: string): string => {
-    return file.startsWith(pwd) ? file.substring(pwd.length) : file
 }
